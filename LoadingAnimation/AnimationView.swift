13e097794f9b3c532167b331721997f1f474f62c
李文静 <Lwj_xyL@163.com>
2018-01-24 10:58:11 +0800

    初始化

---
+++ LoadingAnimation/AnimationView.swift
@@ -0,0 +1,21 @@
+//
+//  AnimationView.swift
+//  LoadingAnimation
+//
+//  Created by 李旭阳 on 2018/1/24.
+//  Copyright © 2018年 李文静. All rights reserved.
+//
+
+import UIKit
+
+class AnimationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
